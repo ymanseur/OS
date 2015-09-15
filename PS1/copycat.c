c956d44488018d35dff37357fefb21dbc24ed5d1
ymanseur <ymanseur94@gmail.com>
2015-09-14 23:48:48 -0400

    Initial work on PS1

---
+++ PS1/copycat.c
@@ -0,0 +1,44 @@
+// Yacine Manseur
+// Cooper Union Fall 2015
+// ECE 357: Operating Systems
+// Problem Set 1
+
+#include <fcntl.h>
+#include <errno.h>
+#include <fctnl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int c = -1;
+	int bufferSize = 256;
+	char* outfile = NULL;
+
+	// Read Command Line Arguments
+	while ((c=getopt(argc, argv, "b:o:")) != -1)
+	{
+		switch (c)
+		{
+			case 'b':
+				bufferSize = atoi(optarg);
+				break;
+			case 'o':
+				outfile = optarg;
+				break;
+			// if getopt() does not recognize an option character
+			case '?':
+				if (optopt == 'b' || optopt == 'c')
+					fprintf(stderr, "Usage: -%c incorrect/missing argument.\n", optopt);
+				else
+					fprintf(stderr, "Usage: %s [-b ###] [-o outfile] infile1 [...infile2....]\n", argv[0]);
+				return -1;
+				break;
+			default:
+				fprintf(stderr, "Usage: %s [-b ###] [-o outfile] infile1 [...infile2....]\n", argv[0]);
+				return-1;
+		}
+	}
+
+	return 0;
+}
