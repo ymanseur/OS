ae7681a769de52f84265ac0ee8fa429216daeaed
Yacine Manseur <ymanseur94@gmail.com>
2015-11-15 01:27:15 -0500

    Working on PS7...

--- PS7/sem.c
+++ PS7/sem.c
@@ -3,3 +3,80 @@
 // ECE 357: Operating Systems
 // Problem Set 7
 // sem.c
+
+#include "sem.h"
+
+// Signal Handler{
+void sig_handler(int signo){}
+
+void sem_init(struct sem *s, int count)
+{
+	s->lock = 0;
+	s->semval = count;
+}
+
+int sem_try(struct sem *s)
+{
+	while(tas(&(s->lock)) != 0);
+
+	if (s->semval > 0)
+	{
+		(s->semval)--;
+		s->lock = 0;
+		return 1;
+	}
+	else
+	{
+		s->lock = 0;
+		return 0;
+	}
+}
+
+void sem_wait(struct sem *s)
+{
+	while(1)
+	{
+		while(tas(&(s->lock)) != 0);
+
+		if(s->semval > 0)
+		{
+			(s->semval)--;
+			s->semwait[my_procnum] = 0;
+			s->lock = 0;
+			return;
+		}
+		else
+		{
+			sigset_t mask;
+			s->semwait[my_procnum] = 1;
+			s->sempid[my_procnum] = getpid();
+			sigfillset(&mask);
+			sigdelset(&mask, SIGUSR1);
+			sigdelset(&mask, SIGINT);
+			sigprocmask(SIG_BLOCK, &mask, NULL);
+			signal(SIGUSR1, sig_handler);
+			s->lock = 0;
+			s->semwait[my_procnum] = 1;
+			sigsuspend(&mask);
+			sigprocmask(SIG_UNBLOCK, &mask, NULL);
+		}
+	}
+}
+
+void sem_inc(struct sem *s)
+{
+	int id;
+	while(tas(&(s->lock)) != 0);
+	s->semval++;
+	for(id = 0; id < N_PROC; id++)
+	{
+		if(s->semwait[id])
+		{
+			s->semwait[id] = 0;
+			kill(s->sempid[id],SIGUSR1);
+			break;
+		}
+	}
+	s->lock = 0;
+}
+
--- PS7/sem.h
+++ PS7/sem.h
@@ -3,3 +3,37 @@
 // ECE 357: Operating Systems
 // Problem Set 7
 // sem.h
+
+#ifndef SEM_H
+#define SEM_H
+
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#define N_PROC 64 /* Maximum number of virtual processors required to accept */
+
+int my_procnum; /* small integer identifier */
+
+struct sem
+{
+	volatile char lock;	  /* lock */
+	int semval;			  /* semaphore value */
+	int semwait[N_PROC];  /* waiting */
+	pid_t sempid[N_PROC]; /* ID of process that did last op */
+};
+
+int tas(volatile char *lock);
+
+// Initialize the semaphore *s with the initial count.
+void sem_init(struct sem *s, int count);
+
+// Attempt to atomically decrement the semaphore
+int sem_try(struct sem *s);
+
+// Atomically decrement the semaphore, blocking until successful.
+void sem_wait(struct sem *s);
+
+// Wake other processes
+void sem_inc(struct sem *s);
+
+#endif
