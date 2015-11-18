55a4e3b83a3fe00d5c2924d14d9e1016935cf242
Yacine Manseur <ymanseur94@gmail.com>
2015-11-17 23:19:47 -0500

    Finished PS8

---
+++ PS8/hello.c
@@ -0,0 +1,16 @@
+// Yacine Manseur
+// Cooper Union Fall 2015
+// ECE 357: Operating Systems
+// Problem Set 8
+// Problem 1
+// hello.c
+// gcc hello.c -o p1
+// strace ./p1
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Hello World.\n");
+	return 0;
+}
